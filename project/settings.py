f484dd11bb51c6cffee7ff8e744d4097d1891606
Ishak <ishak.ariif@gmail.com>
2025-02-13 08:44:48 +0000

    Remove test info

--- project/settings.py
+++ project/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = 'ishak.ariif@gmail.com'  # Gmail address
-EMAIL_HOST_PASSWORD = 'yute xjkd mafz ohbf'  # Gmail app password (not regular password)
-DEFAULT_FROM_EMAIL = 'ishak.ariif@gmail.com'
+EMAIL_HOST_USER = 'example@gmail.com'  # Gmail address
+EMAIL_HOST_PASSWORD = 'gmail_app_pass'  # Gmail app password (not regular password)
+DEFAULT_FROM_EMAIL = 'example@gmail.com'
 
 
