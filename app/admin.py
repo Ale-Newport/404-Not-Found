7d462ecef66cd1d65aa1c806d77a2f38eb046dfb
Alejandro Newport Diaz <alejandro.newport_diaz@kcl.ac.uk>
2025-02-17 19:09:49 +0000

    seeding jobs

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from app.models import User, Job
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'user_type')
+    list_filter = ('user_type',)
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+
+@admin.register(Job)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'salary', 'job_type', 'bonus', 'skills_needed', 'skills_wanted', 'created_at', 'created_by')
+    list_filter = ('job_type',)
+    search_fields = ('name', 'department', 'salary', 'skills_needed', 'skills_wanted', 'created_by')
+    ordering = ('name',)
